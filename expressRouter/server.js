82dde1fcd60c34d388cad6479d568531019d15db
hakeemadnan <hakeemadnan86@gmail.com>
2026-05-05 16:33:14 +0530

    flashs and sessions

--- expressRouter/server.js
+++ expressRouter/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js")
 const posts =require("./routes/post.js")
+const flash = require("connect-flash");
 // const cookieParser = require("cookie-parser")
-
 const session = require("express-session");
+const path = require("path");
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 // app.use(cookieParser("secretcode"));
 
 
@@ -38,23 +41,50 @@ const session = require("express-session");
 // //users route
 // app.use("/users",users);
 
-// //posts router
+// // //posts router
 // app.use("/posts",posts);
 
+const sessionOptions = {secret:"mysupersecretstring",resave :false,saveUninitialized:true,};
 //express-session
-app.use(session({secret:"mysupersecretstring",resave :false,saveUninitialized:true,}));
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req,res,next) =>{
+    res.locals.success = req.flash("successMsg");
+    res.locals.error = req.flash("ErrorMsg");
+    next()
+})
+app.get("/register",(req,res) =>{
+    let {name = "annonymous"} = req.query;
+    req.session.name = name;
+
+    // console.log(req.session)
+    if(name == "annonymous"){
+        req.flash("ErrorMsg","user not registered");
+    }else{
+        req.flash("successMsg","user registered successfully");
+    }
+    res.redirect("/hello");
+});
+
+
+app.get("/hello",(req,res) =>{
+    res.render("page.ejs", {name :req.session.name});
+})
 
 // app.get("/test",(req,res) =>{
 //     res.send("test succesfull");
 // })
-app.get("/reqcount",(req,res) =>{
-    if(req.session.count){
-        req.session.count++;
-    }else{
-    req.session.count =1
-    }
-    res.send(`you sent a request ${req.session.count} times`);
-})
+// app.get("/reqcount",(req,res) =>{
+//     if(req.session.count){
+//         req.session.count++;
+//     }else{
+//     req.session.count =1
+//     }
+//     res.send(`you sent a request ${req.session.count} times`);
+// })
+
+
 app.listen(3000, () => {
     console.log("Server running");
   });
